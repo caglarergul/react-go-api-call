8e86de312485524edcf04569b55296fade12fcee
Caglar Ergul <caglarergul@me.com>
2017-12-06 18:02:35 +0300

    Some new pages are created.

---
+++ src/AppAddPerson.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AppAddPerson extends Component {
+    render() {
+        return (
+            <div>
+                Add Person
+            </div>
+        );
+    }
+}
+
+export default AppAddPerson;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppAddPerson from './AppAddPerson'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppAddPerson />, document.getElementById('add-person'));
 registerServiceWorker();
